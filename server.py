4816e1a89424b9ebdfceaeffa7deebb3e0830fb1
autumn-moon-123 <2387105387@qq.com>
2025-05-04 19:49:20 +0800

    second save

--- server.py
+++ server.py
@@ -1,2 +1,10 @@
 import socket
 import threading
+
+class Valuestore:
+    def __init__(self):
+        self.store = {} 
+        self.lock = threading.Lock()
+    def read(self, k):
+        with self.lock:
+         return self.store.get(k, "")
