ec56efabd1cdd860feab235e3569f047705dd6d0
autumn-moon-123 <2387105387@qq.com>
2025-05-05 23:20:36 +0800

    12 save

--- client.py
+++ client.py
@@ -1,5 +1,14 @@
 import socket
 import threading
 
-data = client_socket.recv(1024).decode('utf-8')
-print(f"\n{data}")
+def receive():
+    while True:
+        data = client_socket.recv(1024).decode('utf-8')
+        print(f"\n{data}")
+
+    open(filename, 'r') as file:
+            for line in file:
+                command = line.strip()
+                if command:
+                    print(f"[Sending] {command}")
+                    client_socket.send(command.encode('utf-8'))
