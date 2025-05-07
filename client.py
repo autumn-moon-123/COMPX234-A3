623a17b537808f93a1b4118b7b2794920d5019bc
autumn-moon-123 <2387105387@qq.com>
2025-05-07 18:19:41 +0800

    18 save

--- client.py
+++ client.py
@@ -8,17 +8,17 @@ def receive():
 
 def send_commands(filename):
     try:
-        with open(filename, 'r') as file:
-            for line in file:
-                command = line.strip()
-                if command:
-                    print(f"[Sending] {command}")
-                    client_socket.send(command.encode('utf-8'))
+        if filename:  
+            with open(filename, 'r', encoding='utf-8') as file:
+                for line in file:
+                    if command := line.strip():
+                        print(f"[Sending] {command}")
+                        client_socket.sendall(command.encode('utf-8'))
+                        time.sleep(0.3)
     except FileNotFoundError:
-        print(f"Error: File '{filename}' not found")
+        print(f"[Error] File '{filename}' not found")
     except Exception as e:
-        print(f"Error reading file: {e}")
-
+        print(f"[Error] {str(e)}")
 
 HOST = '127.0.0.1' 
 PORT = 56789
